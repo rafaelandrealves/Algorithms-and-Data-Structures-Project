7a6fd69e88d79aa4d0482cf073caaca3ebb2d87c
rafaelandrealves <rafaelandrealves@gmail.com>
2018-10-27 20:15:26 +0100

    Já tratei dos argc e do ficheiro, era para fazer mais alguma coisa?  with '#' will be ignored, and an empty message aborts the commit.

--- main.c
+++ main.c
@@ -8,11 +8,26 @@
 
 int main (int argc, char ** argv)
 { 
-    if(strstr(argv[0],tuktuk);
+    FILE *fp=NULL;
+
+    if(strcmp(argv[0],"./tuktuk")!=0 ){
+        printf("ERROR-NAME OF THE EXEC. HAS TO BE tuktuk");
+        exit (1);
+    }
+    if(strstr(argv[1],".cities")==NULL){//será que é preciso garantir que está na posição certa??
+        printf("ERROR-HAS TO BE A .cities file");
+        exit (1);
+    }
+    fp=fopen(argv[1],"r");
+    if(fp==NULL){// validação,caso o ficheiro não abra, o prgrama vai fechar
+        printf("Não foi possível abrir o ficheiro");
+        exit(1);
+    }
 
 
 
 
 
+    fclose(fp);
     return (0);
 }
